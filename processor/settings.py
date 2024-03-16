9cb5f5ebcb55fffe21e72435a14b7e08afe275bd
Tomer <tomerne@mta.ac.il>
2024-03-16 13:55:31 +0200

    mid changes

--- processor/settings.py
+++ processor/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    bootstrap_servers: str = Field("localhost:9092", env="BOOTSTRAP_SERVERS")
+    bootstrap_servers: str = Field("kafka.kafka.svc.cluster.local:9092", env="BOOTSTRAP_SERVERS")
     topics: str = Field("test", env="RECEIVE_DATA_TOPICS")
     group_id: str = Field("group_test", env="GROUP_ID")
 
