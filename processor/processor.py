de9242ea3f3e22f2a376326e3562e226e2a16933
Tomer <tomerne@mta.ac.il>
2024-03-20 23:16:33 +0200

    adding the web app

--- processor/processor.py
+++ processor/processor.py
@@ -1,27 +1,46 @@
 import logging
+import asyncio
+import os
+import socket
 
-from confluent_kafka import Consumer, KafkaException
+from confluent_kafka import Consumer, KafkaException, Producer
 
 from settings import settings
 
 conf = {
     "bootstrap.servers": settings.bootstrap_servers,
-    "group.id": settings.group_id,
+    "client.id": socket.gethostname(),
+    "security.protocol": "SASL_PLAINTEXT",  # Specify the SASL protocol
+    "sasl.mechanism": "PLAIN",  # Specify the SASL mechanism
+    "sasl.username": "user1",  # Your Kafka username
+    "sasl.password": os.getenv("password")  # Your Kafka password
 }
 
+producer = Producer(conf)
 consumer = Consumer(conf)
+consumer.subscribe(settings.collector_topic)
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-topics = settings.topics.split()
-consumer.subscribe(topics)
 
-
-def process_message(decoded_msg):
+async def calculate_anomaly(cpu, memory):
     pass
 
 
-def main():
+async def process_message(decoded_msg):
+    logger.info("process message %s", decoded_msg)
+    cpu, memory = decoded_msg["cpu"], decoded_msg["memory"]
+    if cpu <= 0 or memory <= 0:
+        logger.error("message does not valid %s", decoded_msg)
+        return
+    if await calculate_anomaly(cpu, memory):
+        logger.info("sending message %s", decoded_msg)
+        producer.produce(topic=settings.anomaly_topic, value=decoded_msg.encode("utf-8"))
+        logger.info("sent message %s", decoded_msg)
+
+
+async def main():
     while True:
         try:
             logger.info("polling")
@@ -35,7 +54,7 @@ def main():
 
             decoded_msg = msg.value().decode("utf-8")
             logger.info("new message %s", decoded_msg)
-            process_message(decoded_msg)
+            await process_message(decoded_msg)
 
         except (KeyError, KafkaException) as e:
             logger.error(e)
@@ -46,5 +65,5 @@ def main():
             break
 
 
-if __name__ == "__main__":
-    main()
+logger.info("processor starts to run")
+asyncio.run(main())
--- processor/settings.py
+++ processor/settings.py
@@ -4,8 +4,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     bootstrap_servers: str = Field("kafka.kafka.svc.cluster.local:9092", env="BOOTSTRAP_SERVERS")
-    topics: str = Field("test", env="RECEIVE_DATA_TOPICS")
+    collector_topic: str = Field("test", env="COLLECTOR_TOPIC")
     group_id: str = Field("group_test", env="GROUP_ID")
+    anomaly_topic: str = Field("test2", env="ANOMALY_TOPIC")
 
 
 settings = Settings()
